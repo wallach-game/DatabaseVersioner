d058ac29d6515786f9a137a6ddaccf338361ab43
Jiří Kořenek <jirklym@gmail.com>
2021-07-12 17:01:46 +0200

    Loader nearlzy finisted, but its donig dump stuff

--- bin/LoadCommit.sh
+++ bin/LoadCommit.sh
@@ -12,13 +12,48 @@ source ./config/cfg.config
 echo 
 echo
 
-echo "SELECT database commit";
+echo "SELECT database commit to load";
 
 databasecommits=()
-counter=0
-for file in ../commits/*.tar.gz
+cd ../commits
+for file in *.tar.gz
 do 
     databasecommits=(${databasecommits[@]} "$file")
 done
 
-echo ${databasecommits[@]}
+echo "*Index* *Commit"
+for i in "${!databasecommits[@]}"; do 
+  printf "%s\t%s\n" "*[$i]" " ${databasecommits[$i]}"
+done
+
+echo "Select commit and press enter"
+echo
+read -n1
+SELECTEDCOMMIT=$REPLY
+SELECTEDCOMMIT=$SELECTEDCOMMIT
+COMMITSLENGHT=${#databasecommits[@]}
+
+
+
+# if 
+#     then
+#         echo "Selected Index out of array boundary, exiting"
+#         exit 1
+# fi
+echo "Selected Commit: " ${databasecommits[$SELECTEDCOMMIT]}
+echo
+echo "Do you wanna overwrite container "$CONTAINERID" with "${databasecommits[$SELECTEDCOMMIT]}
+echo
+echo "press any key to continue or CTRL+C to shutdown script"
+read
+echo "CLEANING ENVIROMENT"
+echo ""
+rm ../output/* -drf
+echo "EXTRACTING COMMIT"
+cd ../commits
+
+echo ${databasecommits[$SELECTEDCOMMIT]}
+pwd
+echo "tar -xfv "${databasecommits[$SELECTEDCOMMIT]}" -C ../../output"
+tar -xfv ${databasecommits[$SELECTEDCOMMIT]} -C ../../output
+cd ../bin
