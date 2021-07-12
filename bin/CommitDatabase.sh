8ab485a7e158a58973e983d03057d5d25435c3d4
Jiří Kořenek <jirklym@gmail.com>
2021-07-12 13:47:54 +0200

    first commit, also DB commit fce finished, TODO do db restoration

---
+++ bin/CommitDatabase.sh
@@ -0,0 +1,34 @@
+echo "*****************";
+echo "DATABSE VERSIONER";
+echo "     v0.0.1      ";
+echo "*****************";
+
+echo "Currently db have to specifed in this file you are running";
+echo "if you want to not have to press button every time please comment out these lines"
+echo "Press any key to continue";
+read ;
+
+
+#like this
+# echo "Currently db have to specifed in this file you are running";
+# echo "if you want to not have to press button every time please comment out these lines"
+# echo "Press any key to continue";
+# read ;
+
+CONTAINERID="02b3c87c4dde";
+DATABASENAME="retail";
+
+SHACOMMITSUMLONG=$(echo $DATABASENAME$(date)| openssl sha256);
+SHACOMMITSUM=${SHACOMMITSUMLONG: -10};
+
+#TODO move downward echo $SHACOMMITSUM;
+
+ARCHIVENAME=$SHACOMMITSUM$DATABASENAME;
+
+#docker cp $CONTAINERID:/var/lib/mysql/$DATABASENAME ../output
+echo "DATABASE EXTRACTION FINISTED";
+echo "DATABASE PACKAGING STARTED";
+tar -czvf ../commits/$ARCHIVENAME.tar.gz ../output
+echo "DATABASE PACKAGING FINISHED";
+rm ../output/* -rdf
+echo "ENVIROMENT CLEARED"
